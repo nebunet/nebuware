844b1ee6cf880e61e30adfdd6fb2a7a3b0f503cd
nebunet <109931844+nebunet@users.noreply.github.com>
2022-11-10 21:47:57 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,33 +1,33 @@
-		local scripts = {
-			["baller"] = "",
-			["ush"] = "https://raw.githubusercontent.com/nebunet/untitled-script-hub/main/main.lua",
-			["swords"] = "https://pastebin.com/raw/UAuVm0pu",
-			["taunts"] = "https://raw.githubusercontent.com/nebunet/fe-taunts-assets/main/source.lua",
-		}
+local scripts = {
+	["baller"] = "",
+	["ush"] = "https://raw.githubusercontent.com/nebunet/untitled-script-hub/main/main.lua",
+	["swords"] = "https://pastebin.com/raw/UAuVm0pu",
+	["taunts"] = "https://raw.githubusercontent.com/nebunet/fe-taunts-assets/main/source.lua",
+}
 
-		local loader = {}
+local loader = {}
 
-		function loader.Loadstring(url)
-			loadstring(game:HttpGet(url))()
+function loader.Loadstring(url)
+	loadstring(game:HttpGet(url))()
+end
+
+function loader.GetUrl(script, t)
+	for _, v in pairs(t) do
+		if t[script] then
+			return t[script]
 		end
+		warn('Couldn\'t find script "'..script..'"" in table!')
 
-		function loader.GetUrl(script, t)
-			for _, v in pairs(t) do
-				if t[script] then
-					return t[script]
-				end
-				warn('Couldn\'t find script "'..script..'"" in table!')
+		return false
+	end
+end
 
-				return false
-			end
-		end
+function loader:LoadScript(name)	
+	local url = loader.GetUrl(name, scripts)	
 
-		function loader:LoadScript(name)	
-			local url = loader.GetUrl(name, scripts)	
+	if not url then return end
 
-			if not url then return end
+	loader.Loadstring(url)	
+end
 
-			loader.Loadstring(url)	
-		end
-
-		return loader
+return loader
